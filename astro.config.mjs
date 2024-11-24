506d0a07886a234794acfef910a87080303fd40c
suhankins <punishedbernadetta@niuitmo.ru>
2024-11-24 20:04:16 +0400

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,34 @@ import rehypeSlug from 'rehype-slug';
 import paragraphToFigure from './src/rehype/paragraph-to-figure';
 import replaceWebmImgWithVideo from './src/rehype/replace-webm-img-with-video';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://detta.dev',
-    markdown: {
-        remarkPlugins: [
-            [remarkToc, { heading: 'Contents', maxDepth: 3, ordered: true }],
-        ],
-        rehypePlugins: [
-            rehypeSlug,
-            [
-                rehypeAutolinkHeadings,
-                {
-                    behavior: 'prepend',
-                    content: {
-                        type: 'element',
-                        tagName: 'span',
-                        properties: {},
-                        children: [{ type: 'text', value: '#' }],
-                    },
-                },
-            ],
-            replaceWebmImgWithVideo,
-            paragraphToFigure,
-        ],
-    },
+  site: 'https://detta.dev',
+
+  markdown: {
+      remarkPlugins: [
+          [remarkToc, { heading: 'Contents', maxDepth: 3, ordered: true }],
+      ],
+      rehypePlugins: [
+          rehypeSlug,
+          [
+              rehypeAutolinkHeadings,
+              {
+                  behavior: 'prepend',
+                  content: {
+                      type: 'element',
+                      tagName: 'span',
+                      properties: {},
+                      children: [{ type: 'text', value: '#' }],
+                  },
+              },
+          ],
+          replaceWebmImgWithVideo,
+          paragraphToFigure,
+      ],
+  },
+
+  integrations: [sitemap()],
 });
